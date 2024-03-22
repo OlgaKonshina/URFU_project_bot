9577c74248ad0f7b5afd5d20d33b79d2baecf3e8
Olga <konshinaolga32@gmail.com>
2024-03-22 09:36:10 +0300

    Update модель .py

--- модель .py
+++ модель .py
@@ -7,6 +7,6 @@
 import whisper
 
 model = whisper.load_model('small')
-result = model.transcribe('audio_6.ogg') # добавляем аудио для обработки
+result = model.transcribe('audio_6.ogg', fp16=False) # добавляем аудио для обработки
 print(result['text'])
 
