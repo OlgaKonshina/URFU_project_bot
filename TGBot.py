207904bf5923ebd7217d5122177b5296ca864ab9
VasiliVorobev <vvorobiev@bk.ru>
2024-04-14 17:45:04 +0300

    В код программы добавлены проверки

--- TGBot.py
+++ TGBot.py
@@ -6,7 +6,6 @@ import subprocess
 import pytz
 import yaml
 
-
 with open(r'TGBot_config.yaml', 'r') as f:
     config = yaml.safe_load(f)
 
@@ -23,72 +22,64 @@ def start_message(message):
 
 @bot.message_handler(commands=['help'])
 def help_message(message):
-    bot.send_message(message.chat.id, 'Этот бот переводит голосовые сообщения в текст\nБот создан в учебных целях\n\nThis bot translates voice messages into text\nThe bot was created for educational purposes.')
+    bot.send_message(message.chat.id, 'Этот бот переводит голосовые сообщения в текст\nБот создан в учебных '
+                                      'целях\n\nThis bot translates voice messages into text\nThe bot was created for'
+                                      ' educational purposes.')
 
 
 @bot.message_handler(
-    content_types=['audio', 'photo', 'video', 'video_note','document', 'text', 'location', 'contact', 'sticker'])
+    content_types=['audio', 'photo', 'video', 'document', 'text', 'location', 'contact', 'sticker'])
 def exceptions(message):
     bot.send_message(message.from_user.id,
-                     "Ничего не понятно, но очень интересно!😳\nПопробуйте команду /help\n\nNothing is clear, but it is very interesting!😳 \nTry the /help command😳")
+                     "Ничего не понятно, но очень интересно!😳\nПопробуйте команду /help\n\nNothing is clear, "
+                     "but it is very interesting!😳 \nTry the /help command😳")
 
 
-@bot.message_handler(content_types=['voice'])
-def get_audio_messages(message):
+@bot.message_handler(content_types=['voice', 'video_note'])
+def get_media_messages(message):
     bot.send_message(message.from_user.id, "Started recognition...")
-    # Основная функция, принимает голосовое сообщение от пользователя
     try:
         bot.send_message(message.from_user.id, "Continue recognition...")
-        # Ниже пытаемся вычленить имя файла, да и вообще берем данные с мессаги
-        file_info = bot.get_file(message.voice.file_id)
-        print('file_info = ', file_info)
-        path = file_info.file_path  # Вот тут-то и полный путь до файла (например: voice/file_2.oga)
-        fname = os.path.basename(path)  # Преобразуем путь в имя файла (например: file_2.oga)
-        print(fname)
-        doc = requests.get('https://api.telegram.org/file/bot{0}/{1}'.format(token,
-                                                                             file_info.file_path))  # Получаем и сохраняем присланную голосовуху
-        with open(fname + '.oga', 'wb') as f:
-            f.write(doc.content)  # вот именно тут и сохраняется сама аудио-мессага
-        #print(fname)
+        file_id = message.voice.file_id if message.content_type == 'voice' else message.video_note.file_id
+        file_info = bot.get_file(file_id)
+        path = file_info.file_path
+        fname = os.path.basename(path)
+        doc = requests.get('https://api.telegram.org/file/bot{0}/{1}'.format(token, file_info.file_path))
+        with open(fname, 'wb') as f:
+            f.write(doc.content)
 
         try:
-            subprocess.run(['ffmpeg', '-i', fname + '.oga', fname + '.wav'], check=True)
+            subprocess.run(['ffmpeg', '-i', fname, fname[:-4] + '.wav'], check=True)
         except subprocess.CalledProcessError as e:
             bot.send_message(message.from_user.id,
                              "c конвертацией файла что-то пошло не так... 😣")
-            os.remove(fname + '.oga')
+            os.remove(fname)
             return
 
         model = whisper.load_model('small')
-        # print('model = ', model)
-        bot.send_message(message.from_user.id, 'Загрузили модель\nLoaded the model')
+        bot.send_message(message.from_user.id, 'Model loaded')
 
         try:
-            result = model.transcribe(fname + '.wav', fp16=False)  # распознаем аудио и переводим в текст
+            result = model.transcribe(fname[:-4] + '.wav', fp16=False)  # распознаем аудио и переводим в текст
         except Exception as e:
-           bot.send_message(message.from_user.id,
-                          "Что-то пошло не так c распознованием😣")
-           os.remove(fname + '.oga')
-           os.remove(fname + '.wav')
+           bot.send_message(message.from_user.id,"Что-то пошло не так c распознованием😣")
+           os.remove(fname)
+           os.remove(fname[:-4] + '.wav')
            return
 
         bot.send_message(message.from_user.id, "Finish recognition...")
 
         if result['text'] == '':
-            bot.send_message(message.from_user.id,
-                             "Ничего не удалось распознать 😣")
+            bot.send_message(message.from_user.id,"Ничего не удалось распознать 😣")
         else:
             bot.send_message(message.from_user.id, result['text'])
 
     except Exception as e:
-           bot.send_message(message.from_user.id,
-                          "Что-то пошло не так, но наши смелые инженеры уже трудятся над решением... 😣  \nSomething went wrong, but our brave engineers are already working on a solution... 😣")
-
+        bot.send_message(message.from_user.id,
+                         "Что-то пошло не так, но наши смелые инженеры уже трудятся над решением... 😣  \nySomething went wrong, but our brave engineers are already working on a solution... 😣")
     finally:
-        os.remove(fname + '.oga')
-        os.remove(fname + '.wav')
-        pass
+        os.remove(fname)
+        os.remove(fname[:-4] + '.wav')
 
 
 bot.polling(none_stop=True, interval=0)
-
